5e8a8a14dddffcec6e845196b295770d1f0c2089
Niinuma.Toshitaka <niinuma.toshitaka@gmail.com>
2020-04-05 11:41:50 +0900

    音声再生処理を関数化

--- src/playVoice.py
+++ src/playVoice.py
@@ -1,18 +1,36 @@
 import subprocess
 import random
 
-# 状態ごとの音声ファイル一覧
+
 voiceDataDir = "../voice/"
+"""音声ファイルの格納ディレクトリ"""
 voiceDataFiles = {
     "morning": ["おはようございます.wav"],
     "go_out": ["今日もお仕事頑張ってきてくださいね.wav", "行ってらっしゃい.wav"],
     "back": ["おかえりなさい.wav"],
     "night": ["おやすみなさい.wav"],
 }
+"""状態ごとの音声ファイル一覧"""
 
-# リストからランダムに音声を指定
-voiceFile = voiceDataDir + random.choice(voiceDataFiles["go_out"])
 
-# 音声を再生
-command = "aplay " + voiceFile
-subprocess.call(command, shell=True)
+def play_voice(state: str) -> None:
+    """現在の状態に対応する音声を再生する。
+
+    Args:
+        state (str): 現在の状態
+
+    Returns:
+        None
+    """
+    # リストからランダムに音声を指定
+    voice_file = voiceDataDir + random.choice(voiceDataFiles[state])
+
+    # 音声を再生
+    command = "aplay " + voice_file
+    subprocess.call(command, shell=True)
+
+    return
+
+
+if __name__ == '__main__':
+    play_voice("go_out")
