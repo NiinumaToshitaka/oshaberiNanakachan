9938521a4dc872d98d83fc91cc3ae7b90c17244f
Niinuma.Toshitaka <niinuma.toshitaka@gmail.com>
2020-04-05 12:27:18 +0900

    テキストからの音声合成処理を関数化

--- src/requestToVoiceText.py
+++ src/requestToVoiceText.py
@@ -1,23 +1,45 @@
 import src.VoiceTextWebAPIKey as Key
 import requests
 
+
 obtainedVoiceSavePath = '../voice/{}.wav'
+"""合成した音声を保存するディレクトリ"""
 requestURL = 'https://api.voicetext.jp/v1/tts'
+"""APIのリクエストURL"""
 
-parameter = {
-    'speaker': 'hikari',
-    'pitch': 120,
-    'emotion': 'happiness',
-    'emotion_level': 2,
-}
-data = parameter
 
-data['text'] ='おはようございます'
+def request_to_voice_text(data: dict) -> None:
+    """リクエストパラメータを基に音声を合成する。
 
-response = requests.post(requestURL, data=data, auth=(Key.API_KEY, ''))
+    Args:
+        data (dict): APIに投げるリクエストパラメータ
 
-if response.status_code is requests.codes.ok:
-    with open(obtainedVoiceSavePath.format(data['text']), 'wb') as f:
-        f.write(response.content)
-else:
-    print("[ERROR] status_code: " + response.status_code)
+    Returns:
+        None
+    """
+    response = requests.post(requestURL, data=data, auth=(Key.API_KEY, ''))
+
+    if response.status_code is requests.codes.ok:
+        with open(obtainedVoiceSavePath.format(data['text']), 'wb') as f:
+            f.write(response.content)
+            print("finish.")
+    else:
+        print("[ERROR] status_code: {}".format(response.status_code))
+
+
+if __name__ == '__main__':
+    parameter = {
+        'text': 'そろそろ寝なくちゃダメですよ',
+        'speaker': 'hikari',
+        'format': 'wav',
+        'emotion': 'happiness',
+        # 'emotion_level': 2,
+        'pitch': 120,
+        'speed': 100,
+        'volume': 100,
+    }
+    """APIに投げるリクエストパラメータ。    
+    各パラメータの詳細は<https://cloud.voicetext.jp/webapi/docs/api>を参照
+    """
+
+    request_to_voice_text(parameter)
