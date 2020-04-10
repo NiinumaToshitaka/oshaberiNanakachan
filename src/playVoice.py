32aadb07ab34441035fd6068ab284f28290adccc
Niinuma.Toshitaka <niinuma.toshitaka@gmail.com>
2020-04-10 22:23:38 +0900

    天気予報データベースから天気情報を取得できなかったときに再生するエラーボイスを追加

--- src/playVoice.py
+++ src/playVoice.py
@@ -13,14 +13,12 @@ voiceDataFiles = {
     "go_out": ["今日もお仕事頑張ってきてください", "行ってらっしゃい"],
     "back": ["おかえりなさい"],
     "night": ["おやすみなさい"],
+    "unknown": ["ごめんなさい、ちょっとよくわかりません"],
+    "weather": ["天気予報"],
+    "badWeather": ["傘を持つのを忘れないでくださいね"],
+    "failedToGetWeatherData": ["ごめんなさい、天気情報を取得できませんでした"],
 }
 """状態ごとの音声ファイル一覧"""
-voiceData_unknownInputMessage = "ごめんなさい、ちょっとよくわかりません"
-"""対応する状態が存在しない場合のエラー通知音声"""
-voiceData_weather = "天気予報"
-"""天気予報を読み上げる音声ファイル"""
-voiceData_badWeather = "傘を持つのを忘れないでくださいね"
-"""悪い天気のときに傘を持つよう警告する音声ファイル"""
 bad_weather = ['雨', '雪']
 """悪い天気を表すテキスト"""
 
@@ -53,7 +51,7 @@ def play_voice(state: str) -> None:
         None
     """
 
-    voice_file = voiceDataDir.format(voiceData_unknownInputMessage)
+    voice_file = voiceDataDir.format(random.choice(voiceDataFiles["unknown"]))
     """再生する音声ファイルのパス"""
     # 現在の状態に対応する音声ファイルを取得
     if state in voiceDataFiles:
@@ -62,16 +60,15 @@ def play_voice(state: str) -> None:
     # 天気予報を読み上げる音声ファイルを取得
     elif state is "weather_today":
         if get_weather_forecast_voice(datetime.datetime.now()):
-            voice_file = voiceDataDir.format(voiceData_weather)
+            voice_file = voiceDataDir.format(random.choice(voiceDataFiles["weather"]))
         else:
-            # [TODO] 天気予報データを取得できなかった場合は、その旨を伝えるボイスを再生する
+            voice_file = voiceDataDir.format(random.choice(voiceDataFiles["failedToGetWeatherData"]))
             pass
     elif state is "weather_tomorrow":
         if get_weather_forecast_voice(datetime.datetime.now() + datetime.timedelta(days=1)):
-            voice_file = voiceDataDir.format(voiceData_weather)
+            voice_file = voiceDataDir.format(random.choice(voiceDataFiles["weather"]))
         else:
-            # [TODO] 天気予報データを取得できなかった場合は、その旨を伝えるボイスを再生する
-            pass
+            voice_file = voiceDataDir.format(random.choice(voiceDataFiles["failedToGetWeatherData"]))
 
     # 音声を再生
     play_voice_file(voice_file)
@@ -85,7 +82,7 @@ def play_voice(state: str) -> None:
             if weather in weather_forecast_data['telop']:
                 is_bad_weather = True
         if is_bad_weather:
-            play_voice_file(voiceDataDir.format(voiceData_badWeather))
+            play_voice_file(voiceDataDir.format(random.choice(voiceDataFiles["badWeather"])))
 
     return
 
@@ -126,7 +123,7 @@ def get_weather_forecast_voice(date: datetime.datetime) -> bool:
 
     # 天気予報データを読み上げるボイスを合成
     request_data = {'text': weather_voice_base_string}
-    requestToVoiceText.request_to_voice_text(request_data, voiceData_weather)
+    requestToVoiceText.request_to_voice_text(request_data, random.choice(voiceDataFiles["weather"]))
 
     return True
 
