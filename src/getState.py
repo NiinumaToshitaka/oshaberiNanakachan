1a1265b2c583843c631e96d2db2183c727ba6640
Niinuma Toshitaka <niinuma.toshitaka@gmail.com>
2020-05-23 14:28:18 +0900

    七香ぺでぃあの音声入出力処理を追加

--- src/getState.py
+++ src/getState.py
@@ -1,39 +1,51 @@
 import difflib
 
 
-def get_state(input_message: str) -> str:
-    """
-    入力メッセージに対応する状態を返す。
-
-    Args:
-        input_message (str): 入力メッセージ
-
-    Returns:
-        state (str): 入力メッセージに対応する状態名
+class GetState:
+    """状態を扱うクラス
     """
 
-    min_matching_ratio = 0.75
+    MIN_MATCHING_RATIO = 0.75
     """入力メッセージと規定コマンドの最小類似度。もっとも類似度の高い規定コマンドの類似度がこの値以下の場合は認識不能とする。"""
-    state = "unknown"
-    """入力メッセージに対応する状態"""
-    states = {
+    STATES = {
         "morning": "おはよう",
         "go_out": "行ってきます",
         "back": "ただいま",
         "night": "おやすみ",
         "weather_today": "今日の天気は",
         "weather_tomorrow": "明日の天気は",
+        "KnowTheWikipediaTitle": "知ってる",
+        "DoNotKnowTheWikipediaTitle": "知らない",
     }
     """状態名とコマンドの対応一覧"""
 
-    closest_state = max(states.items(), key=lambda x: difflib.SequenceMatcher(None, input_message, x[1]).ratio())
-    matching_ratio = difflib.SequenceMatcher(None, input_message, closest_state[1]).ratio()
-    print("matching_ratio: {:.2f}".format(matching_ratio))
-    if matching_ratio > min_matching_ratio:
-        state = closest_state[0]
+    def __init__(self):
+        self.state = "unknown"
+        """入力メッセージに対応する状態"""
 
-    return state
+    def get_state(self, input_message: str) -> str:
+        """
+        入力メッセージに対応する状態を返す。
+
+        Args:
+            input_message (str): 入力メッセージ
+
+        Returns:
+            self.state (str): 入力メッセージに対応する状態名
+        """
+
+        closest_state = max(GetState.STATES.items(), key=lambda x: difflib.SequenceMatcher(None, input_message, x[1]).ratio())
+        matching_ratio = difflib.SequenceMatcher(None, input_message, closest_state[1]).ratio()
+        print("matching_ratio: {:.2f}".format(matching_ratio))
+        if matching_ratio > GetState.MIN_MATCHING_RATIO:
+            self.state = closest_state[0]
+
+        return self.state
+
+
+def test():
+    print("state: {}".format(GetState().get_state("知らない")))
 
 
 if __name__ == '__main__':
-    print("state: {}".format(get_state("おやすみー")))
+    test()
--- src/oshaberiNanakaChan.py
+++ src/oshaberiNanakaChan.py
@@ -10,10 +10,42 @@ import src.getState as getState
 def main():
     text = voice2text.listen()
     print("input_message: " + text)
-    state = getState.get_state(text)
+    state = getState.GetState().get_state(text)
     print("state: " + state)
     playVoice.play_voice(state)
 
 
+def do_nanakapedia():
+    """七香ぺでぃあ機能を実行
+    """
+    # [TODO] 人感センサと連動してこの機能を実行する
+
+    np = playVoice.Nanakapedia()
+    # Wikipediaのタイトルをランダムに読み上げる
+    np.ask_does_know_the_title()
+
+    # 音声を入力
+    text = voice2text.listen()
+    print("input_message: " + text)
+
+    # 入力された音声に対応する状態を取得
+    np_state = getState.GetState()
+    np_state.get_state(text)
+    print("state: " + np_state.state)
+
+    # タイトルを知っている場合
+    if np_state.state == "KnowTheWikipediaTitle":
+        np.has_known_the_title = True
+        np.play_the_ask_result()
+    # タイトルを知らない場合
+    elif np_state.state == "DoNotKnowTheWikipediaTitle":
+        np.has_known_the_title = False
+        np.play_the_ask_result()
+    # 状態を判定できなかった場合
+    else:
+        playVoice.play_voice(np_state.state)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    do_nanakapedia()
